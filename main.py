ad4271f260f4a2136757f93a0923d8a4cfad833c
Victor Maciel <victormaciel.dev@gmail.com>
2024-05-22 00:11:16 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from tkinter import*
+from tkinter import tk, StringVar, tkk
+
+#cores
+
+co0 = "2e2d2b"  #preto
+co1 = "#feffff" #branco
+co2 = "#4fa882" #verde
+co3 = "#38576b" #valor
+co4 = "#403d3d" #letra
+co5 = "#e06636" # - profit
+co6 = "038cfc"  #azul
+co7 = "#3fbfb9" #verde
+co8 = "263238"  # + verde
+co9 = "e9edf5"  # + verde
+
+# Criando janela
+
+janela = Tk()
+janela.title('')
+janela.geometry('900x600')
+janela.configure(background=co9)
+janela.resizable(width=FALSE, height=FALSE)
+
+janela.mainloop()
