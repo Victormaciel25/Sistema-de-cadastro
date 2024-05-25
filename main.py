93338a6611d93fe5c700fa222b8008969d5de8f1
Victor Maciel <victormaciel.dev@gmail.com>
2024-05-25 01:04:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
-from tkinter import StringVar
+from tkinter import StringVar, Frame
 from tkinter import ttk
+
+from PIL import Image, ImageTk
 
 # Cores
 co0 = "#2e2d2b"  # preto
@@ -20,5 +22,23 @@ janela.title('Exemplo de tkinter')
 janela.geometry('900x600')
 janela.configure(background=co9)
 janela.resizable(width=False, height=False)
+
+style = ttk.Style(janela)
+style.theme_use("clam")
+
+# Criando frames
+
+frameCima = Frame(janela, width=1043, height=50, bg=co1, relief=tk.FLAT)
+frameCima.grid(row=0, column=0)
+
+frameMeio = Frame(janela, width=1043, height=303, bg=co1, pady=20, relief=tk.FLAT)
+frameMeio.grid(row=1, column=0, pady=1, padx=0, sticky=tk.NSEW)
+
+frameBaixo = Frame(janela, width=1043, height=300, bg=co1, relief=tk.FLAT)
+frameBaixo.grid(row=2, column=0, pady=0, padx=1, sticky=tk.NSEW)
+
+# Abrindo imagem
+
+
 
 janela.mainloop()
